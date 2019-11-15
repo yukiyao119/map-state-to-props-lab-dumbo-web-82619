f9c1b15920c69c758117d185f1854436dc4071c3
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-11-15 21:37:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+<<<<<<< HEAD
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+=======
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+>>>>>>> e824459f69c61461757e7baa4ede2dd2b4a30086
 import manageUsers from './reducers/manageUsers'
 
 
 // add imports and code
+<<<<<<< HEAD
 const store = createStore(manageUsers)
+=======
+const store = createStore(manageUsers);
+
+>>>>>>> e824459f69c61461757e7baa4ede2dd2b4a30086
 
 ReactDOM.render(
   // add imports and code
   <Provider store={store}>
+<<<<<<< HEAD
   <App />
   </Provider>
+=======
+  <App /></Provider>
+>>>>>>> e824459f69c61461757e7baa4ede2dd2b4a30086
   // add imports and code
   ,
   document.getElementById('root')
